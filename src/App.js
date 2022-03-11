19a2ab3fc59aad91477961e45cc46ae0e4b0fdb8
mrin2810 <mrinmayeerane2810@gmail.com>
2022-03-11 16:46:16 +0530

    hide highlight on mouse out

--- src/App.js
+++ src/App.js
@@ -10,12 +10,19 @@ function App() {
       left: e.nativeEvent.layerX - 150,
     })
   }
+  
+  function hideHighlight(e) {
+    setHighlightStyle({
+      opacity: 0,
+      left: e.nativeEvent.layerX - 150,
+    })
+  }
 
   return (
     <div className="app">
       <div className="browser">
         <div className="tabs">
-          <div className="tab" onMouseMove={moveHighlight}>
+          <div className="tab" onMouseMove={moveHighlight} onMouseOut={hideHighlight}>
             <div className='highlight' style={highlightStyle}/>
             <a href='localhost://3000'>Home</a>
           </div>
